df37db53b73498ffc572c080caf4671cfab7879c
Sergey Opasen <ykcyc13@gmail.com>
2023-09-24 18:02:39 +0200

    hw-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,64 @@
-import Phonebook from './Phonebook';
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Phonebook />
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleFilterChange = filter => {
+    this.setState({ filter });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          gap: '5px',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 30,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-class Phonebook extends Component {
+
+class ContactForm extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
     name: '',
     number: '',
   };
@@ -18,27 +11,15 @@ class Phonebook extends Component {
   handleNumberChange = e => {
     this.setState({ number: e.target.value });
   };
-  handleFilterChange = e => {
-    this.setState({ filter: e.target.value.toLowerCase() });
-  };
   handleSubmit = e => {
     e.preventDefault();
-    const newContact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
+    this.props.onAddContact(this.state.name, this.state.number);
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
-    }));
+    this.setState({ name: '', number: '' });
   };
   render() {
     return (
-      <div>
-        <h1>Phonebook</h1>
+      <>
         <form
           onSubmit={this.handleSubmit}
           style={{
@@ -48,10 +29,10 @@ class Phonebook extends Component {
             flexDirection: 'column',
           }}
         >
-          Name
           <input
             type="text"
             name="name"
+            placeholder="Name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -61,35 +42,17 @@ class Phonebook extends Component {
           <input
             type="tel"
             name="number"
+            placeholder="Phone number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             value={this.state.number}
             onChange={this.handleNumberChange}
           />
-          <input
-            type="text"
-            name="filter"
-            placeholder="Start to tape your contact`s name"
-            value={this.state.filter}
-            onChange={this.handleFilterChange}
-          />
           <button type="submit">Add contact</button>
         </form>
-        <h2>Contatcs</h2>
-        <ul>
-          {this.state.contacts
-            .filter(contact =>
-              contact.name.toLowerCase().includes(this.state.filter)
-            )
-            .map(contact => (
-              <li key={contact.id}>
-                {contact.name}: {contact.number}
-              </li>
-            ))}
-        </ul>
-      </div>
+      </>
     );
   }
 }
-export default Phonebook;
+export default ContactForm;
